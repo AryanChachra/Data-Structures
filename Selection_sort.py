b48cce69a345e17bbda5532c733ebf89aea61eb4
Aryan Chachra <aryanchachra1406@gmail.com>
2024-10-04 22:02:16 +0530

    Add files via upload

---
+++ Selection_sort.py
@@ -0,0 +1,25 @@
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        minl=i
+        for j in range(i+1,n):
+            if arr[j]<arr[minl]:
+                minl=j
+        
+        if minl != i:
+            arr[i], arr[minl] = arr[minl], arr[i]
+
+
+
+def main():
+    arr=[]
+    n=int(input('Enter the size of the array: '))
+    for i in range(n):
+        val=input()
+        arr.append(val)
+    print(arr)
+    selection_sort(arr)
+    print(arr)
+
+if __name__=="__main__":
+    main()
