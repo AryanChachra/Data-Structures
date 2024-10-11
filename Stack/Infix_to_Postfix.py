de9e207d81dc5d653469bec29cfcf8a896e85d2d
Aryan Chachra <aryanchachra1406@gmail.com>
2024-10-11 14:53:47 +0530

    Update Infix_to_Postfix.py

--- Stack/Infix_to_Postfix.py
+++ Stack/Infix_to_Postfix.py
@@ -8,17 +8,12 @@ def precedence(c):
     else:
         return -1
 
-def associativity(c):
-    if c=='^':
-        return 'R'
-    return 'L'
-
 def infix_to_postfix(s):
     result=[]
     stack=[]
-    for i in range(len(s)):
-        c=s[i]
-        if ('a'<= c <= 'z') or ('A'<= c <='Z') or ('0' <= c <= '9'):
+    
+    for c in s:
+        if ('a'<= c <='z') or ('A'<= c <='Z') or ('0'<=c<='9'):
             result.append(c)
         elif c=='(':
             stack.append(c)
@@ -27,17 +22,21 @@ def infix_to_postfix(s):
                 result.append(stack.pop())
             stack.pop()
         else:
-            while stack and (precedence(s[i]) < precedence(stack[-1]) or precedence(s[i])==precedence(stack[-1]) and associativity(s[i])=='L'):
+            while (stack and stack[-1] != '(' and 
+                   (precedence(c) < precedence(stack[-1]) or 
+                    (precedence(c) == precedence(stack[-1]) and c != '^'))):
                 result.append(stack.pop())
             stack.append(c)
-    while stack:
+        
+    while stack :
         result.append(stack.pop())
     
     print(''.join(result))
 
+
 def main():
     s=input()
     infix_to_postfix(s)
-
-if __name__=="__main__":
+    
+if __name__=='__main__':
     main()
